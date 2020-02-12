29669202e64965aa8d11422b923975b9fc1cbc9c
Marcelo Agoston <marceloagoston91@gmail.com>
2020-02-12 13:21:36 -0300

    basic templates and basic register/login functionalities done

---
+++ sga_cet/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include 
+from django.views.generic.base import TemplateView 
+
+urlpatterns = [
+	path('admin/', admin.site.urls),
+	path('usuario/', include('user.urls')), 
+	path('usuario/', include('django.contrib.auth.urls')),
+	path('', include('pages.urls')),
+]
